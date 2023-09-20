6818783f20ad66835d1ffb54622d0fe73fc203aa
Ann Tarek <68911302+AnnTarek@users.noreply.github.com>
2023-09-20 17:19:38 +0300

    main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # load a pretrained model
 
 # Train the model with 1 GPU
-results = model.train(data='data.yaml', epochs=80, imgsz=225, device=0)
+results = model.train(data='data.yaml', epochs=80, imgsz=256, device=0)
 pre = model.predict('triangle.jpeg', save=True, show=True, conf=0.8) #predict an image
